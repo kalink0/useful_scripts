db844a83d0606631df66808e8eaa856df10d96ef
kalinko <kalinko@be-binary.de>
2019-07-27 20:07:12 +0200

    Added SQL for Antox Contacts

---
+++ sql/Android/Antox/antox_contacts.sql
@@ -0,0 +1,31 @@
+/*
+ * AUTHOR:         kalink0
+ * MAIL:           kalinko@be-binary.de
+ * CREATION DATE:  2019/07/27
+ *
+ * LICENSE:        CC0-1.0
+ *
+ * SOURCE:         https://github.com/kalink0/useful_scripts/iOS/Antox
+ *
+ * TITLE:          antox_contacts.sql
+ *
+ * DESCRIPTION:    Script to parse contacts out of Antox Messenger database.
+ *								 Location 			- /data/data/chat.tox.antox/databases
+ *								 Database name 	- [antox_username].db
+ *
+ *
+ * KNOWN RESTRICTIONS:
+ *
+ *
+ * USAGE EXAMPLE:  Execute with e.g. SQlite Browser or SQlite CLI on the sqlite file IMODb2_1.sqlite
+ *
+ */
+
+
+ SELECT tox_key AS "User Key",
+ 	name AS 'Username',
+ 	CASE WHEN isblocked is '1'
+ 		THEN 'Blocked'
+ 		ELSE 'Not Blocked'
+ 	END AS 'Blocked Status'
+ FROM contacts;
