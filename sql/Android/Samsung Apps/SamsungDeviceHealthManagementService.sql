c852bbbf236e7a2617fa9f447a856aa71391d662
kalink0 <kalinko@be-binary.de>
2026-01-10 19:08:01 +0100

    Aded CPUSTATS query

--- sql/Android/Samsung Apps/SamsungDeviceHealthManagementService.sql
+++ sql/Android/Samsung Apps/SamsungDeviceHealthManagementService.sql
@@ -7,6 +7,7 @@
 -- Parse Samsung Device Health Management Service App
 -- Author: Marco Neumann (kalink0)
 
+-- USAGE LOG AND STATS
 -- List Network Usage
 -- Data for about the last 5 days
 -- If Package Name == -5 -> Whole system data - not app specific
@@ -35,3 +36,17 @@ chg_temp/10.0 [Charging IC Temperature],
 pa_temp/10.0 [Cellular Radio Temperature],
 wifi_temp/10.0 [WiFi Temperature]
 FROM TEMPERATURE
+
+-- List CPU Usage
+-- Data for about the last 5 days
+-- Process Usage is about factor 1000 for 100 % of 1 Core, e.g. 1000 = 1 Core by 100 %, 2000 = 2 Cores by 100 %
+-- Database Name: thermal_log
+SELECT
+strftime('%Y-%m-%d %H:%M:%S.', "start_time"/1000, 'unixepoch') || ("start_time"%1000) [Start Time],
+strftime('%Y-%m-%d %H:%M:%S.', "end_time"/1000, 'unixepoch') || ("end_time"%1000) [End Time],
+uptime [Uptime],
+process_name [Process Name],
+uid [Package ID],
+pid [Process ID],
+process_usage [Process Usage]
+FROM CPUSTAT
