7d0e6bb2116c54c022cfd41e9096c8c52988719c
kalink0 <kalinko@be-binary.de>
2026-02-25 20:25:31 +0100

    modified sql query romeo accounts info

--- sql/Android/Romeo/romeo_account_info.sql
+++ sql/Android/Romeo/romeo_account_info.sql
@@ -1,7 +1,7 @@
 -- App: Android ROMEO - Gay Dating
 -- App Version: 3.28.3
 -- Creation Date: 2025-11-16
--- Last Modification Date: 2025-11-16
+-- Last Modification Date: 2026-02-25
 -- Database Location: data\data\com.planetromeo.android.app\databases\
 -- Database Name: accounts.db
 -- Parse ROMEO Accounts Infos
@@ -15,5 +15,11 @@ email [E-Mail],
 json_extract(location, '$.address.address') [Address],
 json_extract(location, '$.name') [City],
 json_extract(location, '$.lat') [Latitude],
-json_extract(location, '$.long') [Longitude]
+json_extract(location, '$.long') [Longitude],
+json_extract(profile, '$.headline') [Headline],
+json_extract(profile, '$.personal.profile_text') [Profile Text],
+json_extract(profile, '$.creation_date') [Creation Date],
+json_extract(profile, '$.last_login') [Last Login],
+json_extract(profile, '$.personal.age') [Age],
+json_extract(profile, '$.personal.birthdate') [Birthdate]
 FROM accounts
